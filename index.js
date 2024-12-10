50fcd584ba72686297f8709b4f39d9f358fca55f
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-10 14:19:50 +0600

    connect to db with mongoose

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 require('dotenv').config()
 
-const app = express()
-//middleware 
+//middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Routes
+app.get('/', () => {
+    res.send('Hello World!')
 })
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+
+
+//Database Connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    })
+    .catch((err) => console.error('Error connecting to database:', err));
